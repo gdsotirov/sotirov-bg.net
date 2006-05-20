e3ada227e73611d0a2dc34d3e11d4d9717813f54
Georgi D. Sotirov <gdsotirov@dir.bg>
2006-05-20 23:34:20 +0000

    Continue fixing validation problems RSS

--- news/feed.bg.pl
+++ news/feed.bg.pl
@@ -2,7 +2,7 @@
 # This script is intended to localize the news feeds of Sotirov-BG.Net
 # Written by Georgi D. Sotirov <gdsotirov@dir.bg>
 #
-# $Id: feed.bg.pl,v 1.1 2006/05/20 18:02:40 gsotirov Exp $
+# $Id: feed.bg.pl,v 1.2 2006/05/20 23:34:20 gsotirov Exp $
 #
 
 use strict;
@@ -15,9 +15,10 @@ my $feed_lang_id = "bg";
 my $feed_title = "Sotirov-BG.Net Новини";
 my $feed_desc = "Новини от Sotirov-BG.Net";
 my $feed_link = "http://sotirov-bg.net/news";
-my $feed_copy = "Copyright &copy; 2004-2006 Георги Д. Сотиров";
+my $feed_self = "http://sotirov-bg.net/news/feed";
+my $feed_copy = "Copyright (c) 2004-2006 Георги Д. Сотиров";
 my $feed_author_name = "Георги Д. Сотиров";
-my $feed_author_uri = "http://gsotirov79.ddns.homelan.bg/~gsotirov/";
+my $feed_author_uri = "http://sotirov-bg.net/~gsotirov/";
 my $feed_author_email = 'gsotirov@dir.bg';
 my $feed_editor_name = 'Георги Д. Сотиров';
 my $feed_editor_email = 'gsotirov@dir.bg';
--- news/feed.en.pl
+++ news/feed.en.pl
@@ -2,7 +2,7 @@
 # This script is intended to localize the news feeds of Sotirov-BG.Net
 # Written by Georgi D. Sotirov <gdsotirov@dir.bg>
 #
-# $Id: feed.en.pl,v 1.1 2006/05/20 18:02:41 gsotirov Exp $
+# $Id: feed.en.pl,v 1.2 2006/05/20 23:34:20 gsotirov Exp $
 #
 
 use strict;
@@ -15,9 +15,10 @@ my $feed_lang_id = "en";
 my $feed_title = "Sotirov-BG.Net News";
 my $feed_desc = "News from Sotirov-BG.Net";
 my $feed_link = "http://sotirov-bg.net/news";
-my $feed_copy = "Copyright &copy; 2004-2006 Georgi D. Sotirov";
+my $feed_self = "http://sotirov-bg.net/news/feed";
+my $feed_copy = "Copyright (c) 2004-2006 Georgi D. Sotirov";
 my $feed_author_name = "Georgi D. Sotirov";
-my $feed_author_uri = "http://gsotirov79.ddns.homelan.bg/~gsotirov/";
+my $feed_author_uri = "http://sotirov-bg.net/~gsotirov/";
 my $feed_author_email = 'gsotirov@dir.bg';
 my $feed_editor_name = 'Georgi D. Sotirov';
 my $feed_editor_email = 'gsotirov@dir.bg';
