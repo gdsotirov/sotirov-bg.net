5b9b4e6d990771bf45575fc8171d42b645de35aa
Georgi D. Sotirov <gdsotirov@gmail.com>
2019-05-21 21:59:46 +0300

    Do not use zero dates

--- db/sotirov_net.sql
+++ db/sotirov_net.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.10
+-- MySQL dump 10.13
 --
 -- Host: localhost    Database: sotirov_net
 -- ------------------------------------------------------
--- Server version	5.0.26-log
+-- Server version	5.7.26-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+ SET NAMES utf8 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -20,43 +20,53 @@
 --
 
 DROP TABLE IF EXISTS `news`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `news` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `title` varchar(100) NOT NULL default '',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL DEFAULT '',
   `body` text NOT NULL,
   `author` int(10) unsigned NOT NULL,
-  `posted` timestamp NOT NULL default '0000-00-00 00:00:00',
-  `created` timestamp NOT NULL default '0000-00-00 00:00:00',
-  `updated` timestamp NOT NULL default '0000-00-00 00:00:00',
-  PRIMARY KEY  (`id`,`author`),
+  `posted` timestamp NULL DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`,`author`),
   KEY `author_key` (`author`),
   KEY `title_idx` (`title`),
   KEY `time_idx` (`posted`,`created`,`updated`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='News archive';
-
---
--- Definition of trigger `news_insbf`
---
-
-DELIMITER $$
-
-CREATE DEFINER = `root`@`localhost` TRIGGER  `sotirov_net`.`news_insbf` BEFORE INSERT ON `news` FOR EACH ROW BEGIN
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8 COMMENT='News archive';
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER  `sotirov_net`.`news_insbf` BEFORE INSERT ON `news` FOR EACH ROW BEGIN
   SET NEW.created = NOW();
 
   UPDATE users
      SET posts = posts + 1
    WHERE id = NEW.author;
-END $$
-
+END */;;
 DELIMITER ;
-
---
--- Definition of trigger `news_updbf`
---
-
-DELIMITER $$
-
-CREATE DEFINER = `root`@`localhost` TRIGGER  `sotirov_net`.`news_updbf` BEFORE UPDATE ON `news` FOR EACH ROW BEGIN
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER  `sotirov_net`.`news_updbf` BEFORE UPDATE ON `news` FOR EACH ROW BEGIN
   SET NEW.updated = NOW();
 
   IF OLD.author <> NEW.author THEN
@@ -68,74 +78,103 @@ CREATE DEFINER = `root`@`localhost` TRIGGER  `sotirov_net`.`news_updbf` BEFORE U
        SET posts = posts - 1
      WHERE id = OLD.author;
   END IF;
-END $$
-
+END */;;
 DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
--- Table structure for table `news`
+-- Table structure for table `news_bg`
 --
+
 DROP TABLE IF EXISTS `news_bg`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `news_bg` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `title` varchar(100) NOT NULL default '',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL DEFAULT '',
   `body` text NOT NULL,
   `author` int(10) unsigned NOT NULL,
-  `posted` timestamp NOT NULL default '0000-00-00 00:00:00',
-  `created` timestamp NOT NULL default '0000-00-00 00:00:00',
-  `updated` timestamp NOT NULL default '0000-00-00 00:00:00',
-  PRIMARY KEY  (`id`,`author`),
+  `posted` timestamp NULL DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`,`author`),
   KEY `author_key` (`author`),
   KEY `title_idx` (`title`),
   KEY `time_idx` (`posted`,`created`,`updated`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='News archive in Bulgarian';
-
---
--- Definition of trigger `news_bg_insbf`
---
-
-DELIMITER $$
-
-CREATE DEFINER = `root`@`localhost` TRIGGER  `sotirov_net`.`news_bg_insbf` BEFORE INSERT ON `news_bg` FOR EACH ROW BEGIN
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8 COMMENT='News archive in Bulgarian';
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER  `sotirov_net`.`news_bg_insbf` BEFORE INSERT ON `news_bg` FOR EACH ROW BEGIN
   SET NEW.created = NOW();
-END $$
-
+END */;;
 DELIMITER ;
-
---
--- Definition of trigger `news_bg_updbf`
---
-
-DELIMITER $$
-
-CREATE DEFINER = `root`@`localhost` TRIGGER  `sotirov_net`.`news_bg_updbf` BEFORE UPDATE ON `news_bg` FOR EACH ROW BEGIN
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER  `sotirov_net`.`news_bg_updbf` BEFORE UPDATE ON `news_bg` FOR EACH ROW BEGIN
   SET NEW.updated = NOW();
-END $$
-
+END */;;
 DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `users`
 --
+
 DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `users` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `name` varchar(60) NOT NULL default '',
-  `name_bg` varchar(60) NOT NULL default '',
-  `firstname` varchar(60) NOT NULL default '',
-  `firstname_bg` varchar(60) NOT NULL default '',
-  `email` varchar(256) NOT NULL default '',
-  `registered` timestamp NOT NULL default '0000-00-00 00:00:00',
-  `nickname` varchar(30) default NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) NOT NULL DEFAULT '',
+  `name_bg` varchar(60) NOT NULL DEFAULT '',
+  `firstname` varchar(60) NOT NULL DEFAULT '',
+  `firstname_bg` varchar(60) NOT NULL DEFAULT '',
+  `email` varchar(256) NOT NULL DEFAULT '',
+  `registered` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `nickname` varchar(30) DEFAULT NULL,
   `picture` blob,
-  `phone` varchar(64) default '',
-  `posts` int(10) unsigned NOT NULL default '0',
-  PRIMARY KEY  (`id`),
+  `phone` varchar(64) DEFAULT '',
+  `posts` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
   KEY `name_idx` (`name`),
   KEY `fname_idx` (`firstname`),
   KEY `name_bg_idx` (`name_bg`),
   KEY `firstname_bg_idx` (`firstname_bg`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Users table';
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='Users table';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping events for database 'sotirov_net'
+--
+
+--
+-- Dumping routines for database 'sotirov_net'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -145,3 +184,5 @@ CREATE TABLE `users` (
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-05-21 21:59:18
